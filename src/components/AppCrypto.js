327fe65df1c9c5d434486319303f8ca38e74bf02
venky424 <venkateshdwarapureddi0789@gmail.com>
2019-03-15 19:27:33 +0100

    Updated fetch to get alll the currencies from application layer

--- src/components/AppCrypto.js
+++ src/components/AppCrypto.js
@@ -1,15 +1,61 @@
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
-import {Jumbotron,Button} from 'react-bootstrap';
+import {Jumbotron,Button,Table} from 'react-bootstrap';
 
 class AppCrypto extends Component{
+
+  constructor(props) {
+  super(props);
+  this.state = {currencies: null};
+}
+
+
+
+componentDidMount() {
+  const requestOptions = {
+      method: 'GET',
+      headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*' }
+  };
+   fetch('http://localhost:8080/secretdata/allcurrency/',requestOptions)
+        .then(results => {
+          return results.json();
+        })
+        .then(data => {
+          let currencies =data.results.map((curr) =>{
+            return(
+              <Table>
+                 <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>CURRENCY_NAME</th>
+                    <th>CURRENCY_TICKER</th>
+                  </tr>
+                 </thead>
+                 <tbody>
+                    <tr>
+                       <td>{curr.id} </td>
+                       <td>{curr.currencyname}  </td>
+                       <td>{curr.currencyticker}  </td>
+                    </tr>
+                 </tbody>
+              </Table>
+            )
+          })
+          this.setState({currencies:currencies});
+          console.log("state",this.state.currencies);
+})
+
+}
+
   render(){
     return(
-      <div className="container-fluid">
-        <h1>Ciao , Crypto!</h1>
-        <p>
-        <Button variant="primary">Learn more</Button>
-        </p>
+      <div className="container2">
+        <div className="container1">
+         {this.state.currencies}
+        </div>
         </div>
 
     );
--- src/components/AppHome.js
+++ src/components/AppHome.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import {Jumbotron,Button} from 'react-bootstrap';
 
 class AppHome extends Component{
+
   render(){
     return(
       <Jumbotron>
         <h1>Ciao , Leute!</h1>
         <p>
-        Place to boost up yourself
+        Place to boost up life
         </p>
         <p>
         <Button variant="primary">Learn more</Button>
