4775b24b6a5b0bbec9e0e44619ad7f7d856706f5
venky424 <venkateshdwarapureddi0789@gmail.com>
2019-03-20 23:15:40 +0100

    further updates to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,49 @@ import ReactDOM from 'react-dom';
 import AppHeader from './AppHeader.js'
 import AppFooter from './AppFooter.js'
 import AppMenuSwitch from './AppMenuSwitch';
+import Responsive from 'react-responsive-decorator';
 
+@Responsive
 class App extends Component{
+
+state={isMobile:false}
+
+componentDidMount (){
+         this.props.media({ minWidth: 768 },() =>{
+           this.setState({
+             isMobile:false
+           });
+         });
+
+         this.props.media({ maxWidth: 768 },() =>{
+           this.setState({
+             isMobile:true
+           });
+         });
+
+}
+
   render(){
+
+     const { isMobile } = this.state;
+
     return (
+      <div>
+      {isMobile ?
+        <div>
+      <div className="Header">
+      <AppHeader />
+      </div>
+      <div className="Appswitch">
+      <br></br>
+      <AppMenuSwitch />
+      <br></br>
+      </div>
+      <div className="Footer">
+      <AppFooter />
+      </div>
+      </div>
+      :
       <div>
       <div className="Header">
       <AppHeader />
@@ -20,6 +59,9 @@ class App extends Component{
       <AppFooter />
       </div>
       </div>
+      }
+      </div>
+
     );
   }
 }
